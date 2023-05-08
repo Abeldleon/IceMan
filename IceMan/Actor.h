225e2e258d21bf97fb3276fb1527a951b83aa41c
johnnyk2000 <108166588+johnnyk2000@users.noreply.github.com>
2023-05-08 07:14:25 -0700

    Update Actor.h
    classes for each actor (only started on Actor and Ice)

--- IceMan/Actor.h
+++ IceMan/Actor.h
@@ -5,4 +5,66 @@
 
 // Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
 
+
+
+class Actor : public GraphObject { // ABC
+public:
+	Actor(int imageID, int startX, int startY, Direction dir, double size, unsigned int depth)
+		: GraphObject(imageID, startX, startY, dir, size, depth) {
+		isActive = true;
+		
+	}
+	virtual void doSomething() = 0;
+	bool getIsActive() { // to check is object is active
+		return isActive;
+	}
+	void setInactive() { // to set an object inactive
+		isActive = false;
+	}
+private:
+	bool isActive; // for move() function in StudentWorld that will check whether each actor is active. Can be modified by each derived class
+};
+
+class Ice : public Actor {
+public:
+	Ice(int imageID, int startX, int startY,Direction dir, double size, unsigned int depth)
+		: Actor(imageID, startX, startY, dir, size, depth) {
+		dir = right;
+		
+	}
+	virtual void doSomething() {
+
+	}
+};
+
+class Boulder : public Actor {
+
+};
+
+class WaterPuddle : public Actor {
+
+};
+
+class Character : public Actor {
+
+};
+
+class Iceman : public Character {
+
+};
+
+class Protestor : public Character {
+
+};
+
+class HardcoreProtestor : public Protestor {
+
+};
+
+class RegularProtestor : public Protestor {
+
+};
+
+
+
 #endif // ACTOR_H_
