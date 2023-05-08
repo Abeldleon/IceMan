f31525be8e91a67a861eb2f6dbdc1a270fe39d4e
johnnyk2000 <108166588+johnnyk2000@users.noreply.github.com>
2023-05-08 07:12:32 -0700

    Update StudentWorld.cpp
    added populateIce() function definition

--- IceMan/StudentWorld.cpp
+++ IceMan/StudentWorld.cpp
@@ -8,3 +8,14 @@ GameWorld* createStudentWorld(string assetDir)
 }
 
 // Students:  Add code to this file (if you wish), StudentWorld.h, Actor.h and Actor.cpp
+void StudentWorld::populateIce() {
+	for (int i = 0; i < VIEW_WIDTH; i++) {
+		//icePtr[i][0] = new Ice(IID_ICE, )
+		for (int j = 0; j <= ICE_HEIGHT; j++) {
+			if (i >= 30 && i <= 33 && j >= 4 && j <= 59)
+				break;
+			icePtr[i][j] = new Ice(IID_ICE, i, j, GraphObject::right, 0.25, 3);
+			icePtr[i][j]->setVisible(true);
+		}
+	}
+}
