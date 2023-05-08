8317ce8c1c93ca4dae459b5c7e2877c963b440ab
johnnyk2000 <108166588+johnnyk2000@users.noreply.github.com>
2023-05-08 07:11:38 -0700

    Update StudentWorld.h
    created pointers to actors and populateIce() function

--- IceMan/StudentWorld.h
+++ IceMan/StudentWorld.h
@@ -4,8 +4,18 @@
 #include "GameWorld.h"
 #include "GameConstants.h"
 #include <string>
+#include <vector>
+
+#include "Actor.h"
+//#include "GraphObject.h"
 
 // Students:  Add code to this file, StudentWorld.cpp, Actor.h, and Actor.cpp
+
+//class Actor;
+//class Iceman;
+//class Ice;
+
+const int ICE_HEIGHT = 59;
 
 class StudentWorld : public GameWorld
 {
@@ -13,19 +23,28 @@ public:
 	StudentWorld(std::string assetDir)
 		: GameWorld(assetDir)
 	{
+		icemanPtr = nullptr;
+		
 	}
 
 	virtual int init()
 	{
+		// initialize data structures to keep track of game's virtual world
+		// construct a new oil field with ice, barrels, boulder, gold, etc
+		// allocate and insert an iceman objet into the game world
+		populateIce();
+
 		return GWSTATUS_CONTINUE_GAME;
 	}
+	void populateIce();
 
 	virtual int move()
 	{
 		// This code is here merely to allow the game to build, run, and terminate after you hit enter a few times.
 		// Notice that the return value GWSTATUS_PLAYER_DIED will cause our framework to end the current level.
-		decLives();
-		return GWSTATUS_PLAYER_DIED;
+		//decLives();
+		//return GWSTATUS_PLAYER_DIED;
+		return GWSTATUS_CONTINUE_GAME;
 	}
 
 	virtual void cleanUp()
@@ -33,6 +52,9 @@ public:
 	}
 
 private:
+	std::vector<Actor*> actorPtr;
+	Iceman* icemanPtr;
+	Ice* icePtr[VIEW_WIDTH][ICE_HEIGHT];
 };
 
 #endif // STUDENTWORLD_H_
